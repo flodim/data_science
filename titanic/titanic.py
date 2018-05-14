4bed69ba68e454c42895c800d703edf90e22b0bf
flox <florian.hassler1989@gmail.com>
2018-05-14 21:10:47 +0200

    use 2d histograms

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,6 +1,8 @@
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
 
 # read data from csv file
 titanic_data = np.genfromtxt('titanic.dat', delimiter=',', skip_header=1)
@@ -10,6 +12,9 @@ classes = titanic_data[:, 0]
 ages = titanic_data[:, 1]
 sexes = titanic_data[:, 2]
 survived = titanic_data[:, 3]
+
+for i in ages:
+    print(i)
 
 # 3d scatter plot
 fig_3d = plt.figure(1)
@@ -21,9 +26,11 @@ ax.set_zlabel('sex')
 ax.set_title('class vs age vs sex')
 fig_3d.show()
 
+gamma = 2
+
 # class vs age
 fig_class_age = plt.figure(2)
-plt.scatter(classes, ages)
+plt.hist2d(classes,ages)
 plt.xlabel('class')
 plt.ylabel('age')
 plt.title('class vs age')
@@ -31,7 +38,7 @@ fig_class_age.show()
 
 # class vs sex
 fig_class_sex = plt.figure(3)
-plt.scatter(classes, sexes)
+plt.hist2d(classes, sexes)
 plt.xlabel('class')
 plt.ylabel('sex')
 plt.title('class vs sex')
@@ -39,11 +46,10 @@ fig_class_sex.show()
 
 # age vs sex
 fig_age_sex = plt.figure(4)
-plt.scatter(ages, sexes)
+plt.hist2d(ages, sexes)
 plt.xlabel('age')
 plt.ylabel('sex')
 plt.title('age vs sex')
 fig_age_sex.show()
-
 
 
